66167ccda4a6360087bb3ce6f054938a9d70344b
jdenright19 <jdenright19@coe.edu>
2022-10-13 09:30:18 -0500

    I have most of the code working and ready to go Ijust need to work  on the backend

---
+++ src/mainPanel.java
@@ -0,0 +1,207 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class mainPanel {
+    private JPanel sdtPanel;
+    private JButton startButton;
+    private redCirclePanel redCirclePanel1;
+
+    private redCirclePanel3 redCirclePanel4;
+
+    private redSquarePanel redSquarePanel1;
+    private JLabel hitLabel;
+    private JLabel misslabel;
+    private JLabel falabel;
+    private JLabel crlabel;
+    private JTextField txtSpeed;
+    private JLabel Speed;
+    private JLabel Subjectlbl;
+    private JTextField SubjectTxt;
+    private JButton startButton2;
+    private JButton startButton3;
+    private JPanel sdtPanel2;
+    private JSlider slider1;
+    private Timer timer;
+    private int hit;
+
+    private static int numChoicesVal;
+    private int miss;
+    private int falseAlarm;
+
+    private String experiment;
+
+    private String subject;
+    private boolean button1;
+
+    private boolean button2;
+
+    private boolean button3;
+
+    private int timerVal;
+
+    private int timerVal2;
+    private String numChoicesTxt;
+    private int numTimesCorrect;
+
+    private int correctReject;
+    private boolean isPresent3;
+
+    public mainPanel() {
+        button1 = false;
+        button2 = false;
+        button3 = false;
+        timerVal = 150;
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                redCirclePanel1.begin();
+                timer = new Timer(timerVal, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        button1 = true;
+                        numChoicesTxt = txtSpeed.getText();
+                        //numChoicesVal = Integer.parseInt(numChoicesTxt);
+
+                        //System.out.println(numChoicesTxt);
+                        System.out.println("Done");
+                        redCirclePanel1.stop();
+                        timer.stop();
+                        feedback();
+                        subject = SubjectTxt.getText();
+                    }
+                });
+                timer.start();
+            }
+
+        });
+        startButton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                redCirclePanel4.begin();
+                timer = new Timer(timerVal, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        button3 = true;
+                        numChoicesTxt = txtSpeed.getText();
+                        numChoicesVal = Integer.parseInt(numChoicesTxt);
+                        System.out.println(numChoicesTxt);
+                        System.out.println("Done");
+                        redCirclePanel4.stop();
+                        timer.stop();
+                        feedback();
+                        subject = SubjectTxt.getText();
+                    }
+                });
+                timer.start();
+            }
+        });
+        startButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                redSquarePanel1.begin();
+                timer = new Timer(timerVal, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        button2 = true;
+                        numChoicesTxt = txtSpeed.getText();
+                        numChoicesVal = Integer.parseInt(numChoicesTxt);
+                        System.out.println(numChoicesTxt);
+                        System.out.println("Done");
+                        redSquarePanel1.stop();
+                        timer.stop();
+                        feedback();
+                        subject = SubjectTxt.getText();
+                    }
+                });
+                timer.start();
+            }
+        });
+    }
+
+
+
+
+    private void feedback() {
+        if (button1) {
+            isPresent3 = redCirclePanel1.isPresent2();
+            //button1 = false;
+        }
+        if (button3) {
+            isPresent3 = redCirclePanel4.isPresent2();
+            //button3 = false;
+        }
+        if (button2) {
+            isPresent3 = redSquarePanel1.isPresent2();
+            //button2 = false;
+        }
+        int input = JOptionPane.showConfirmDialog(null,
+                "Did you see the shape?", "Select an Option...", JOptionPane.YES_NO_OPTION);
+
+        if (isPresent3 == true){
+            if(input ==JOptionPane.YES_OPTION) {
+                falseAlarm++;
+                timerVal += 25;
+                numTimesCorrect = 0;
+                System.out.println(numTimesCorrect);
+
+            }
+            else  {
+                miss++;
+                timerVal += 25;
+                numTimesCorrect = 0;
+                System.out.println(numTimesCorrect);
+            }
+        } else{
+            if(input ==JOptionPane.YES_OPTION) {
+                hit++;
+                numTimesCorrect += 1;
+                System.out.println(numTimesCorrect);
+            }
+            else  {
+                correctReject++;
+                numTimesCorrect += 1;
+                System.out.println(numTimesCorrect);
+            }
+            if (numTimesCorrect == 10)
+            {
+                if (button1 = true)
+                {
+                    experiment = "Experiment 1";
+                }
+                if (button2 = true)
+                {
+                    experiment = "Experiment 2";
+                }
+                if (button3 = true)
+                {
+                    experiment = "Experiment 3";
+                }
+                timerVal2 = timerVal;
+                System.out.println(timerVal);
+                System.out.println(subject);
+                System.out.println(experiment);
+            }
+        }
+
+        hitLabel.setText(hit + " Hit");
+        misslabel.setText(miss + " Miss");
+        falabel.setText(falseAlarm + " False Alarm");
+        crlabel.setText(correctReject + " Correct Reject");
+    }
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Signal Detection Theory");
+        frame.setContentPane(new mainPanel().sdtPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static int numChoicesVal() {
+        return numChoicesVal;
+    }
+}
